3b64023a591b03e7325b7645b63732f396dd12c5
hariharan.r@isteer.in <hariharan.r@isteer.in>
2019-07-05 00:27:04 +0530

    Initial Push

---
+++ src/lab/servlet/welcome/Welcome.java
@@ -0,0 +1,50 @@
+package lab.servlet.welcome;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Welcome
+ */
+//@WebServlet("/welcome")
+public class Welcome implements Servlet {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+
+		System.out.println("Init method");
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		System.out.println("Welcome to simple servlets");
+
+	}
+}
