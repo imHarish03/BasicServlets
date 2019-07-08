cef0ea4c65baf16279dd7473bab0de2585fbde31
hariharan.r@isteer.in <hariharan.r@isteer.in>
2019-07-09 02:26:46 +0530

    * Adds code to create data at the time of sign up

---
+++ src/lab/servlet/Config/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package lab.servlet.Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+	public static Connection initializeDatabase() throws SQLException, ClassNotFoundException {
+		// Initialize all the information regarding
+		// Database Connection
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlets", "root", "root");
+			return con;
+		} catch (Exception e) {
+			System.out.println("Exception : " + e);
+		}
+		return null;
+	}
+}
---
+++ src/lab/servlet/welcome/SignUp.java
@@ -0,0 +1,79 @@
+package lab.servlet.welcome;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import lab.servlet.Config.DatabaseConnection;
+
+public class SignUp implements Servlet {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		try {
+
+			// Initialize the database
+			Connection con = DatabaseConnection.initializeDatabase();
+
+			// Create a SQL query to insert data into demo table
+			// demo table consists of two columns, so two '?' is used
+			PreparedStatement st = con.prepareStatement("insert into student(FirstName,LastName) values(?, ?)");
+
+			// For the first parameter,
+			// get the data using request object
+			// sets the data to st pointer
+			st.setString(1, request.getParameter("firstName"));
+
+			// Same for second parameter
+			st.setString(2, request.getParameter("lastName"));
+
+			// Execute the insert command using executeUpdate()
+			// to make changes in database
+			st.executeUpdate();
+
+			// Close all the connections
+			st.close();
+			con.close();
+
+			// Get a writer pointer
+			// to display the succesful result
+			PrintWriter out = response.getWriter();
+			out.println("<html><body><b>Successfully Inserted" + "</b></body></html>");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
