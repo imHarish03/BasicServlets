5aeef33c9cbac3b508cbe7ca6e3717a1faceca08
imHarish03 <harishhari302@gmail.com>
2019-08-03 23:07:17 +0530

    * Validate session in the filter based on the servlet request path

--- src/lab/servlet/Filter/ValidateCookieFilter.java
+++ src/lab/servlet/Filter/ValidateCookieFilter.java
@@ -1,54 +1,52 @@
 package lab.servlet.Filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import lab.servlet.Constants.Constants;
 
 public class ValidateCookieFilter implements Filter {
 
+	private ServletContext context;
+	private List<String> excludedUrls;
+
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		System.out.println("***************Filter Invoked**********************");
-		HttpServletRequest servletReq = (HttpServletRequest) request;
-		HttpServletResponse servletRes = (HttpServletResponse) response;
+		HttpServletRequest req = (HttpServletRequest) request;
+		String path = req.getServletPath();
+		// HttpServletResponse res = (HttpServletResponse) response;
 
-		if (!servletReq.getRequestURI().equals(Constants.LOGIN_URI)) {
-
-			Cookie ck[] = servletReq.getCookies();
-			for (int i = 0; i < ck.length; i++) {
-				if (ck[i].getName().equals("Authorization")) {
-
-				}
-
-			}
-			/*
-			 * if (session == null) { servletRes.sendRedirect("index.html"); }
-			 */
+		HttpSession session = req.getSession(false);
+		if (session == null && !excludedUrls.contains(path)) {
+			req.getRequestDispatcher("index.html").include(request, response);
+		} else {
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
 		}
-		chain.doFilter(request, response);
 	}
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
+		this.context = filterConfig.getServletContext();
+		this.context.log("AuthenticationFilter initialized");
+
+		String excludePattern = filterConfig.getInitParameter("excludedUrls");
+		excludedUrls = Arrays.asList(excludePattern.split(","));
 	}
 
 }
