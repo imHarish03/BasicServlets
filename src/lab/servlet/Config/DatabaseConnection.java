6a6fe90e8b5c6a51e55e8b440d21258f0b497a7b
imHarish03 <harishhari302@gmail.com>
2019-08-17 13:26:58 +0530

    formatted

--- src/lab/servlet/Config/DatabaseConnection.java
+++ src/lab/servlet/Config/DatabaseConnection.java
@@ -8,7 +8,6 @@ public class DatabaseConnection {
 
 	public static Connection initializeDatabase() throws SQLException, ClassNotFoundException {
 		// Initialize all the information regarding
-		// Database Connection
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlets", "root", "root");
